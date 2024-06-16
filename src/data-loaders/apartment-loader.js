3dc47f48bdc776e050149c78a577a2d78b15f776
TheProgrammerAndTheTraveller <TheProgrammerAndTheTraveller@users.noreply.github.com>
2024-06-16 10:33:18 +0000

    deploy: e6fcf5fae0f681d9cb2e621e4c2d736c62880a2b

--- src/data-loaders/apartment-loader.js
+++
@@ -1,6 +0,0 @@
-import apartmentcards from "../data/apartmentcards";
-
-export function apartmentloader({ params }) {
-    const apartment = apartmentcards[params.id-1];
-    return { apartment };
-}
